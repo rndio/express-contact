1a67df5776f0c71178636ef10154542e0d828c9a
Rendio Simamora <mail@rndio.my.id>
2024-05-16 15:11:30 +0700

    CICD

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { web } from "./app/web.js";
 import { logger } from "./app/logging.js";
 
-web.listen(3000, () => {
-  logger.info("Server started at http://localhost:3000");
+web.listen(5000, () => {
+  logger.info("Server started at http://localhost:5000");
 });
